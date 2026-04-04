5fc4dace8923f3505287c0d4e579b7e819eafce4
Yael <243678@ids.upchiapas.edu.mx>
2026-04-03 21:10:56 -0600

    Se formabn las actividades y la peticion completa utilizando datos de prueba, funciona

--- src/Contexts/API/Activities/Infrastructure/Controllers/AlgoritmoController.ts
+++ src/Contexts/API/Activities/Infrastructure/Controllers/AlgoritmoController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express';
 import { jwtService } from '../../../../../Shared/Infrastructure/Security/jwtService.js';
-import { AlgorithmResponse } from '../../Domain/Algorithm.js';
+import { Activities, AlgorithmRequest, AlgorithmResponse, Config, ConfigType } from '../../Domain/Algorithm.js';
 
 export class AlgoritmoController {
   private jwt = new jwtService()
@@ -8,14 +8,159 @@ export class AlgoritmoController {
   public async procesarDatos(req: Request, res: Response): Promise<void> {
     try {
       const token = req.headers.authorization;
-      const payload = req.get("payload")
+      const requestData = ConfigType.parse(req.body)
+
+      const mockData: Activities = {
+          "defaultReminders":[
+              {
+                  "method":"popup",
+                  "minutes":15
+              }
+          ],
+          "items":[
+              {
+                  "id":"ETIQUETA 1 MEDIA",
+                  "summary":"tarea 1",
+                  "start":{
+                      "dateTime":"2026-03-23T08:30:00-10:00",
+                      "timeZone":"America/Mexico_City"
+                  },
+                  "end":{
+                      "dateTime":"2026-03-23T10:00:00-10:00",
+                      "timeZone":"America/Mexico_City"
+                  },
+                  "transparency":"transparent",
+                  "reminders":{
+                      "useDefault":true
+                  },
+                  "extras":{
+                      "prioridad":"media",
+                      "etiquetas":{
+                          "etiqueta":1,
+                          "color":"#000000"
+                      }
+                  }
+              },
+              {
+                  "id":"ETIQUETA 1 ALTA",
+                  "summary":"tarea 2",
+                  "start":{
+                      "dateTime":"2026-03-23T11:00:00-10:00",
+                      "timeZone":"America/Mexico_City"
+                  },
+                  "end":{
+                      "dateTime":"2026-03-23T12:00:00-10:00",
+                      "timeZone":"America/Mexico_City"
+                  },
+                  "transparency":"transparent",
+                  "reminders":{
+                      "useDefault":true
+                  },
+                  "extras":{
+                      "prioridad":"alta",
+                      "etiquetas":{
+                          "etiqueta":1,
+                          "color":"#000000"
+                      }
+                  }
+              },
+              {
+                  "id":"ETIQUETA 1 BAJA DATE",
+                  "summary":"tarea 3",
+                  "start":{
+                      "date":"2026-03-24"
+                  },
+                  "end":{
+                      "date":"2026-03-25"
+                  },
+                  "transparency":"transparent",
+                  "reminders":{
+                      "useDefault":true
+                  },
+                  "extras":{
+                      "prioridad":"baja",
+                      "etiquetas":{
+                          "etiqueta":1,
+                          "color":"#000000"
+                      }
+                  }
+              },
+              {
+                  "id":"OPAQUE DATE",
+                  "summary":"tarea 4",
+                  "start":{
+                      "date":"2026-03-25"
+                  },
+                  "end":{
+                      "date":"2026-03-26"
+                  },
+                  "transparency":"opaque",
+                  "reminders":{
+                      "useDefault":true
+                  },
+                  "extras":{
+                      "prioridad":"baja",
+                      "etiquetas":{
+                          "etiqueta":1,
+                          "color":"#000000"
+                      }
+                  }
+              },
+              {
+                  "id":"OPAQUE DATETIME",
+                  "summary":"tarea 5",
+                  "start":{
+                      "dateTime":"2026-03-22T14:00:00-10:00",
+                      "timeZone":"America/Mexico_City"
+                  },
+                  "end":{
+                      "dateTime":"2026-03-22T18:00:00-10:00",
+                      "timeZone":"America/Mexico_City"
+                  },
+                  "transparency":"opaque",
+                  "reminders":{
+                      "useDefault":true
+                  },
+                  "extras":{
+                      "prioridad":"baja",
+                      "etiquetas":{
+                          "etiqueta":1,
+                          "color":"#000000"
+                      }
+                  }
+              },
+              {
+                  "id":"ETIQUETA 1 BAJA DIA DISTINTO",
+                  "summary":"tarea 6",
+                  "start":{
+                      "dateTime":"2026-03-26T12:00:00-10:00",
+                      "timeZone":"America/Mexico_City"
+                  },
+                  "end":{
+                      "dateTime":"2026-03-26T18:00:00-10:00",
+                      "timeZone":"America/Mexico_City"
+                  },
+                  "transparency":"transparent",
+                  "reminders":{
+                      "useDefault":true
+                  },
+                  "extras":{
+                      "prioridad":"baja",
+                      "etiquetas":{
+                          "etiqueta":1,
+                          "color":"#000000"
+                      }
+                  }
+              }
+          ]
+      }
 
       if (!token) {
         res.status(401).json({ error: 'Falta el JWT en la cabecera' });
         return;
       }
 
-      if (!payload){
+      if (!requestData){
         res.status(400).json({ error: 'No se enviaron las actividades' })
         return;
       }
@@ -27,12 +172,26 @@ export class AlgoritmoController {
         return;
       }
 
+      const payload: AlgorithmRequest = {
+        config: requestData,
+        calendar: mockData
+      }
+
+      const requestDataEncoded = this.jwt.encodeData(payload)
+      if (!requestDataEncoded){
+        res.status(500).json({
+          success:false,
+          message:'Ocurrió un error al codificarlos datos.'
+        })
+        return
+      }
+
       const respuestaAlgoritmo = await fetch(`${urlAlgoritmo}/api/sort`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': token,
-          'payload': payload
+          'payload': requestDataEncoded
         }
       });
 
